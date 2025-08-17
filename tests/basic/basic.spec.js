9669701e4f3886186cf27b0e64238d72fb304771
arunjosearcj@gmail.com <arunjosearcj@gmail.com>
2025-08-17 14:13:23 +0530

    test featch

--- tests/basic/basic.spec.js
+++ tests/basic/basic.spec.js
@@ -8,3 +8,6 @@ test("has title", async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/); // Check if the title contains "Playwright"
 });
+
+// testinggg
+// 1. abcc
