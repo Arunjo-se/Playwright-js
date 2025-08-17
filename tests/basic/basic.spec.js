92641984d1e805fa0b1c5362751603ec78f91b29
arunjosearcj@gmail.com <arunjosearcj@gmail.com>
2025-08-17 14:10:34 +0530

    minor fix

--- tests/basic/basic.spec.js
+++ tests/basic/basic.spec.js
@@ -8,7 +8,3 @@ test("has title", async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/); // Check if the title contains "Playwright"
 });
-
-// testing
-
-// fnjne
