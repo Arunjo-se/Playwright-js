4eb4c76133aa5696548f9e17414518e4f26e9e1f
arunjosearcj@gmail.com <arunjosearcj@gmail.com>
2025-08-20 08:39:47 +0530

    test

--- tests/basic/basic.spec.js
+++ tests/basic/basic.spec.js
@@ -9,3 +9,5 @@ test("@sanity has title", async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/); // Check if the title contains "Playwright"
 });
+
+// test
