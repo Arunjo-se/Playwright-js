6802b09a4332e508780cbc4869a083d99c1247cc
arunjosearcj@gmail.com <arunjosearcj@gmail.com>
2025-08-20 08:28:36 +0530

    feat: add test scripts and improve sanity test annotation

--- tests/basic/basic.spec.js
+++ tests/basic/basic.spec.js
@@ -1,7 +1,8 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test(" @sanity has title", async ({ page }) => {
+test("@sanity has title", async ({ page }) => {
+  test.info().annotations.push({ type: "tag", description: "sanity" });
   // Test to check if the page has the correct title
   await page.goto("https://playwright.dev/"); // Navigate to the Playwright website
 
